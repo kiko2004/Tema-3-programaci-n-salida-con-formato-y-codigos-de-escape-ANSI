61ffbe83fb2d3b8e99bc809097071b0f9e3a7993
Kiko <145778771+kiko2004@users.noreply.github.com>
2023-10-18 17:03:40 +0200

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ejercicio1{
+    public static void main(String [] args){
+        Scanner lector = new Scanner(System.in);
+        float num = 0;
+        System.out.println("dime un numero");
+        num = lector.nextFloat();
+        System.out.printf("%.2f", num);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
