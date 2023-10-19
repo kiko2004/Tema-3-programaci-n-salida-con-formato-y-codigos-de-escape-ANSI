b1134ef37d7ae0557f7d452612e6c5da4871a2f5
Kiko <145778771+kiko2004@users.noreply.github.com>
2023-10-19 19:13:57 +0200

    Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -3,10 +3,11 @@ public class Ejercicio3 {
         int num = 9;
             //primer bucle, se ejecuta si i es menor o igual que num para ir aumentando los numeros de uno en uno
             for (int i = 1; i <= num; i++) {
-            //segundo bucle para imprimir espacios en blanco
+           
+          //segundo bucle para imprimir los numeros en orden
             for (int j = 1; j <= num - i; j++) 
                 System.out.print("  ");
-            //tercer bucle para imprimir los numeros en orden
+         //tercer bucle para imprimir espacios en blanco
             for (int k = 1; k <= i; k++) 
                 System.out.print(k + " ");
             
