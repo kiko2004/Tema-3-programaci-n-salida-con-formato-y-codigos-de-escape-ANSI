1cb0eedfdb13f8a59247355ce5ec9323f5a6cd7f
Kiko <145778771+kiko2004@users.noreply.github.com>
2023-10-19 03:58:16 +0200

    ejercicio1.java

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ejercicio1{
+public class Ejercicio1{
     public static void main(String [] args){
         Scanner lector = new Scanner(System.in);
         float num = 0;
