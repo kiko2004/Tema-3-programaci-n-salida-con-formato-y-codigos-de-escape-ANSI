a7c86f306f9e85c59654d168fd5eff6194d41690
Kiko <145778771+kiko2004@users.noreply.github.com>
2023-10-19 17:21:32 +0200

    Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,7 +1,7 @@
 public class Ejercicio3 {
     public static void main(String[] args) {
         int num = 9;
-            //primer bucle, se ejecuta si i es menor o igual que n para ir aumentando los numeros de uno en uno
+            //primer bucle, se ejecuta si i es menor o igual que num para ir aumentando los numeros de uno en uno
             for (int i = 1; i <= num; i++) {
             //segundo bucle para imprimir espacios en blanco
             for (int j = 1; j <= num - i; j++) 
